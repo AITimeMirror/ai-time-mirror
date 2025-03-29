e48e2075e5f9c0a8b739ca75d51a9350dd237b25
kapibaba <jksuan@163.com>
2025-03-29 23:45:45 +0800

    更改了合并后的sql语句，并重新命名

--- supabase/20250329.sql
+++ supabase/20250329.sql
@@ -164,7 +164,7 @@ CREATE TRIGGER on_auth_user_created
     AFTER INSERT ON auth.users
     FOR EACH ROW EXECUTE PROCEDURE public.handle_new_user();
 
-CREATE OR REPLACE TRIGGER "customer" AFTER INSERT OR DELETE ON "public"."users" FOR EACH ROW EXECUTE FUNCTION "supabase_functions"."http_request"('https://ai-aging-pied.vercel.app/api/webhooks/supabase/customer', 'POST', '{"Content-type":"application/json"}', '{}', '1000');
+CREATE OR REPLACE TRIGGER "customer" AFTER INSERT OR DELETE ON "public"."users" FOR EACH ROW EXECUTE FUNCTION "supabase_functions"."http_request"('https://your_domain/api/webhooks/supabase/customer', 'POST', '{"Content-type":"application/json"}', '{}', '1000');
 
 /*
  Permissions
